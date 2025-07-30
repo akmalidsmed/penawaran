34b3a9f79ce01d9ae01d2a724a4d8a8f3fae861a
akmalidsmed <akmalidsmed@gmail.com>
2025-07-30 14:30:24 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -147,8 +147,6 @@ if st.button("\U0001F4E5 Generate Dokumen Penawaran"):
     run.add_text(f"Jakarta, {format_tanggal_indonesia(tanggal)}")
 
     p.paragraph_format.space_after = Pt(0)
-
-    p = doc.add_paragraph(f"\n")
     p.paragraph_format.space_after = Pt(0)
     p = doc.add_paragraph(f"Terima kasih atas kesempatan yang telah diberikan kepada kami. Bersama ini kami mengajukan penawaran harga item untuk unit {nama_unit} di {nama_customer}, sebagai berikut:\n")
     p.paragraph_format.space_after = Pt(0)
